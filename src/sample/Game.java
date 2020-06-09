bf53c384f0f5764991e37e0c6a6b45d6131eba4d
mckam <mckamilosm@gmail.com>
2020-06-09 09:09:38 +0200

    Whole game brought to point when its playable, but still requires to make it possible to rotate shapes.

--- src/sample/Game.java
+++ src/sample/Game.java
@@ -1,14 +1,24 @@
 package sample;
 
 import javafx.animation.Animation;
+import javafx.animation.AnimationTimer;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.input.KeyCode;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+
+import java.sql.Timestamp;
+import java.util.Arrays;
+import java.util.Timer;
+import java.util.stream.Stream;
 
 public class Game {
-    public final static int BOXSIZE=25;
+    public final static int BOXSIZE=30;
+    public final static int BOXOFFSET=1;
     public final static String[] SHAPENAMES =
             {"InvertedL" , "Row", "Triangle", "Box"};
 
@@ -21,17 +31,22 @@ public class Game {
     new Color[]{Color.rgb(255, 0 ,19), Color.rgb(255, 134, 136), Color.rgb(224, 0 ,16)},
     new Color[]{Color.rgb(0, 255, 255), Color.rgb(0, 153, 154), Color.rgb(152, 254, 253)}};
 
-    public final static int XBLOCKS=20;
+    public final static int GAMESPEED = 70;
+    public final static int XBLOCKS=15;
     public final static int WIDTH=BOXSIZE*XBLOCKS;
-    public final static int YBLOCKS=35;
+    public final static int YBLOCKS=20;
     public final static int PANELWIDTH = 200;
     public final static int HEIGHT=BOXSIZE*YBLOCKS;
     public final static Color BOXBORDERCOLOR = Color.BLACK;
-    public final static int BORDERSIZE = 4;
-    public final static int SHADESIZE = 6;
-    public final static int INNERBOXSIZE=BOXSIZE-SHADESIZE-BORDERSIZE;
+    public final static int BORDERSIZE = 3;
+    public final static int SHADESIZE = 4;
+    public final static int INNERBOXSIZE=BOXSIZE-2*SHADESIZE-2*BORDERSIZE;
+    public final static int NEWSHAPEX = (BOXSIZE*((XBLOCKS-1)/2));
+    public final static int NEWSHAPEY = 20;
     public final static Game game = new Game();
+    public final static Color[] BLACKBOXCOLORS = new Color[]{Color.BLACK, Color.GRAY, Color.DARKGRAY};
 
+    private GraphicsContext gc;
     private boolean speedUp = false;
     private int points = 0;
     private Block[][] mesh = new Block[XBLOCKS][YBLOCKS];
@@ -39,20 +54,108 @@ public class Game {
 
     private Shape controledShape;
 
+    public Scene getScene() {
+        return scene;
+    }
+
+    private Scene scene;
+
     public Scene initializeScene(){
         Pane pane = new Pane();
         pane.setId("paneNode");
         Canvas mainCanvas = new Canvas();
         mainCanvas.setWidth(WIDTH+ 200);
         mainCanvas.setHeight(HEIGHT);
+        game.gc = mainCanvas.getGraphicsContext2D();
         pane.getChildren().addAll(mainCanvas);
-        return new Scene(pane, WIDTH + PANELWIDTH, HEIGHT);
+        game.scene = new Scene(pane, WIDTH + PANELWIDTH, HEIGHT);
+        // Controll
+        game.getScene().setOnKeyReleased((e) -> {
+            switch (e.getCode()){
+                case UP:
+                case W:
+                    game.rotate();
+                    break;
+                case DOWN:
+                case S:
+                    game.resetSpeedUp();
+                    break;
+                case LEFT:
+                case A:
+                    game.moveLeft();
+                    break;
+                case RIGHT:
+                case D:
+                    game.moveRight();
+                    break;
+            }
+        });
+        game.getScene().setOnKeyPressed((e) -> {
+            if(e.getCode()== KeyCode.S || e.getCode()== KeyCode.DOWN)
+                game.setSpeedUp();
+        });
+        game.getScene().setOnMouseClicked((e) -> {
+            //TODO CLICKS if necessary
+        });
+        return game.scene;
+    }
+    private void initializeMesh(){
+        for (int i = 0; i < YBLOCKS; i++) {
+            game.mesh[0][i] = new Block(0, i*BOXSIZE);
+            game.mesh[XBLOCKS-1][i] = new Block((XBLOCKS-1)*BOXSIZE, i*BOXSIZE);
+        }
+        for (int i = 1; i < XBLOCKS-1; i++) {
+            game.mesh[i][YBLOCKS-1] = new Block(i*BOXSIZE, (YBLOCKS-1)*BOXSIZE);
+        }
     }
     public void start(){
-        //TODO Animation timer, and initial state of aplication
+        initializeMesh();
+        game.controledShape = generateShape();
+            new AnimationTimer(){
+                private long lastUpdate = System.nanoTime();
+                @Override
+                public void handle(long nanoTime) {
+                    if(System.nanoTime()-this.lastUpdate>1000000000/GAMESPEED){
+                        this.lastUpdate=System.nanoTime();
+                        game.gc.clearRect(0,0, game.gc.getCanvas().getWidth(), game.gc.getCanvas().getHeight());
+                        Stream.of(game.mesh).flatMap(Arrays::stream).forEach((Block b) -> {
+                            if(b!=null){
+                                if(b.isSet()){
+                                    b.drawBlock(game.gc);
+                                }
+                            }
+                        });
+                        for (Block b: game.controledShape.getBlocks()
+                             ) {
+                            if(game.mesh[b.getX()/BOXSIZE][((b.getY())/BOXSIZE)+1]!=null){
+                                for (Block block: game.controledShape.getBlocks()
+                                ) {
+                                    block.setY((block.getY()/BOXSIZE)*BOXSIZE);
+                                    block.setX((block.getX()/BOXSIZE)*BOXSIZE);
+                                    game.mesh[(block.getX()/BOXSIZE)][(block.getY()/BOXSIZE)] = block;
+                                }
+                                game.controledShape = generateShape();
+                                checklines();
+                                break;
+                            }else{
+                                if(game.speedUp){
+                                    b.setY(b.getY()+2);
+                                }else{
+                                    b.setY(b.getY()+1);
+                                }
+                                b.drawBlock(game.gc);
+                            }
+                        }
+                        gc.setFill(Color.BLACK);
+                        gc.setFont(Font.font(28));
+                        gc.fillText("Score: " + String.valueOf(game.points), XBLOCKS*BOXSIZE+10, 30);
+                        game.points++;
+                    }
+                }
+            }.start();
     }
 
-    //TODO rotate shapes , move the shapes around
+    //TODO rotate shapes
     public void rotate() {
     }
     public void setSpeedUp(){
@@ -62,12 +165,60 @@ public class Game {
         game.speedUp=false;
     }
     public void moveLeft(){
-
+        for (Block b: controledShape.getBlocks()
+             ) {
+            if(b.getX()<BOXSIZE){
+               return;
+            }
+            if (game.mesh[(b.getX()/BOXSIZE)-1][b.getY()/BOXSIZE]!=null){
+                return;
+            }
+        }
+        // move left
+        for (Block b: controledShape.getBlocks()){
+            b.setX(b.getX()-BOXSIZE);
+        }
     }
     public void moveRight(){
-
+        for (Block b: controledShape.getBlocks()
+        ) {
+            if(b.getX()>BOXSIZE*(XBLOCKS-1)){
+                return;
+            }
+            if (game.mesh[(b.getX()/BOXSIZE)+1][b.getY()/BOXSIZE]!=null){
+                return;
+            }
+        }
+        // move right
+        for (Block b: controledShape.getBlocks()){
+            b.setX(b.getX()+BOXSIZE);
+        }
     }
-    private void generateFigure(){
-
+    private Shape generateShape(){
+        return new Shape();
     }
+    private void checklines(){
+        for (int row =YBLOCKS-1;row>0;row--) {
+            boolean full = true;
+            for(int nb=1;nb<XBLOCKS-1;nb++){
+                if(game.mesh[nb][row]==null) {
+                    full = false;
+                }
+            }
+            if(full&&!game.mesh[1][row].isBlackBlock()){
+                for (int i = 1; i < XBLOCKS-1; i++) {
+                    game.mesh[i][row]=null;
+                }
+                game.points+=2700;
+                for (int i = row; i > 0; i--) {
+                    for (int j = 1; j < XBLOCKS-1; j++) {
+                        game.mesh[j][i] = game.mesh[j][i-1];
+                        if(game.mesh[j][i]!=null)
+                            game.mesh[j][i].setY(game.mesh[j][i].getY()+BOXSIZE);
+                    }
+                }
+            }
+        }
+    }
+
 }
--- src/sample/Shape.java
+++ src/sample/Shape.java
@@ -4,58 +4,92 @@ import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
 import org.jetbrains.annotations.NotNull;
 
-import static sample.Game.game;
-
 public class Shape {
-    Block a;
-    Block b;
-    Block c;
-    Block d;
+    Block[] blocks = new Block[4];
     Color[] colors;
-    private String name;
-
-    public Shape(Block a, Block b, Block c, Block d, String name) {
-        this.a = a;
-        this.b = b;
-        this.c = c;
-        this.d = d;
+    private final String name;
+    public Shape(){
         this.name = randomShape();
         this.colors = randomColors();
+        constructBlocks();
     }
-    public Shape(){
+    public Block[] getBlocks(){
+        return blocks;
+    }
+    private void constructBlocks() {
+        switch (this.name){
+            case "Row":
+                for(int i=0;i<4;i++){
+                    blocks[i] = new Block(Game.NEWSHAPEX+i*Game.BOXSIZE, Game.NEWSHAPEY, colors);
+                }
+                break;
+            case "Triangle":
+                blocks[0] = new Block(Game.NEWSHAPEX, Game.NEWSHAPEY, colors);
+                for (int i = 1; i < 4; i++) {
+                    blocks[i] = new Block(Game.NEWSHAPEX + (i-1)*Game.BOXSIZE, Game.NEWSHAPEY + Game.BOXSIZE, colors);
+                }
+                break;
+            case "Box":
+                for (int i = 0; i < 4; i++) {
+                    blocks[i] = new Block(Game.NEWSHAPEX + (i%2)*Game.BOXSIZE, Game.NEWSHAPEY+ (i/2)* Game.BOXSIZE , colors);
+                }
+                break;
+            case "InvertedL":
+                for (int i = 0; i < 3; i++) {
+                    blocks[i] = new Block(Game.NEWSHAPEX, Game.NEWSHAPEY + i * Game.BOXSIZE, colors);
+                }
+                blocks[3] = new Block(Game.NEWSHAPEX, Game.NEWSHAPEY + 2 * Game.BOXSIZE, colors);;
+                break;
+        }
+    }
 
-    }
     private String randomShape(){
-        return game.SHAPENAMES[(int)(Math.random() * game.SHAPENAMES.length)];
+        return Game.SHAPENAMES[(int)(Math.random() * Game.SHAPENAMES.length)];
     }
     private Color[] randomColors(){
-        return game.COLORPALETE[(int)(Math.random() * game.COLORPALETE.length)];
+        return Game.COLORPALETE[(int)(Math.random() * Game.COLORPALETE.length)];
     }
 
 }
 class Block{
     private int x, y;
-    private Color[] boxColors;
+    private final Color[] boxColors;
+
+    public boolean isBlackBlock() {
+        return blackBlock;
+    }
+
+    private boolean blackBlock = false;
+    private boolean set=false;
 
     public void drawBlock(@NotNull GraphicsContext gc){
-        gc.setFill(game.BOXBORDERCOLOR);
-        gc.fillRect(this.x, this.y, game.BOXSIZE, game.BOXSIZE);
+        gc.setFill(Game.BOXBORDERCOLOR);
+        gc.fillRect(this.x+Game.BOXOFFSET, this.y+Game.BOXOFFSET, Game.BOXSIZE-2*Game.BOXOFFSET, Game.BOXSIZE-2*Game.BOXOFFSET);
         //upper shadow
         gc.setFill(boxColors[1]);
-        gc.fillPolygon(new double[]{this.x + game.BORDERSIZE, this.x + game.BOXSIZE - 2*game.BORDERSIZE,
-                this.x + game.BOXSIZE - 2*game.BORDERSIZE}, new double[]{this.y + game.BORDERSIZE,
-                this.y + game.BORDERSIZE, this.y + game.BOXSIZE - 2*game.BORDERSIZE}, 3);
+        gc.fillPolygon(new double[]{this.x + Game.BORDERSIZE, this.x + Game.BOXSIZE - Game.BORDERSIZE,
+                this.x + Game.BOXSIZE - Game.BORDERSIZE}, new double[]{this.y + Game.BORDERSIZE,
+                this.y + Game.BORDERSIZE, this.y + Game.BOXSIZE - Game.BORDERSIZE}, 3);
         //bottom shadow
         gc.setFill(boxColors[2]);
-        gc.fillPolygon(new double[]{this.x + game.BORDERSIZE, this.x + game.BORDERSIZE,
-                this.x + game.BOXSIZE - 2*game.BORDERSIZE}, new double[]{this.y + game.BORDERSIZE,
-                this.y + game.BOXSIZE - 2*game.BORDERSIZE, this.y + game.BOXSIZE - 2*game.BORDERSIZE}, 3);
+        gc.fillPolygon(new double[]{this.x + Game.BORDERSIZE, this.x + Game.BORDERSIZE,
+                this.x + Game.BOXSIZE - Game.BORDERSIZE}, new double[]{this.y + Game.BORDERSIZE,
+                this.y + Game.BOXSIZE - Game.BORDERSIZE, this.y + Game.BOXSIZE - Game.BORDERSIZE}, 3);
         // inner box
         gc.setFill(boxColors[0]);
-        gc.fillRect(this.x + game.BORDERSIZE + game.SHADESIZE, this.y + game.BORDERSIZE + game.SHADESIZE,
-                game.INNERBOXSIZE, game.INNERBOXSIZE);
+        gc.fillRect(this.x + Game.BORDERSIZE + Game.SHADESIZE, this.y + Game.BORDERSIZE + Game.SHADESIZE,
+                Game.INNERBOXSIZE, Game.INNERBOXSIZE);
+    }
+    public Block(int x, int y, Color[] colors){
+        this.x = x;
+        this.y = y;
+        this.boxColors = colors;
+        this.set=true;
     }
     public Block(int x, int y){
+        this.boxColors = Game.BLACKBOXCOLORS;
+        this.blackBlock = true;
+        this.set=true;
         this.x = x;
         this.y = y;
     }
@@ -74,5 +108,9 @@ class Block{
 
     public void setY(int y) {
         this.y = y;
+    }
+
+    public boolean isSet() {
+        return set;
     }
 }
